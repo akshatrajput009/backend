994d0084f0cfaa8af41fbecb4ebc291eecacdfee
jane <jane@gmail.com>
2023-10-01 12:28:59 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,64 @@
+const express = require("express");
+const http = require("http");
+require("dotenv").config();
+const nodemailer = require("nodemailer");
+const cors = require("cors");
+
+const corsOptions = {
+  origin: "https://651064470c4ca4759a2d6c5b--tiny-madeleine-119d47.netlify.app", // Replace with your client's domain
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  optionsSuccessStatus: 204, // Some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+
+const app = express();
+app.use(cors());
+
+const server = http.Server(app);
+
+const port = 3000;
+app.set("port", port);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//Routing
+
+app.get("/hi", (req, res) => {
+  res.status(200).json({ message: "hello akshat" });
+  res.send("hello");
+});
+
+app.post("/sendEmail", (req, res) => {
+  let { name, email, subject, message } = req.body;
+
+  let transporter = nodemailer.createTransport({
+    host: process.env.HOST_NAME,
+    port: process.env.SMPT_PORT,
+    secure: true,
+    auth: {
+      user: process.env.USER_KEY,
+      pass: process.env.PASS_KEY,
+    },
+  });
+
+  let mailOptions = {
+    from: process.env.USER_KEY,
+    to: process.env.USER_KEY,
+    subject: subject,
+    text: `name : ${name}
+      email: ${email}
+      message: ${message} `,
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      return res.status(404).json({ message: "an error has occurred" });
+    } else {
+      return res.status(200).json({ message: "Message sent successfully! " });
+    }
+  });
+});
+
+server.listen(port, () => {
+  console.log("starting server on port: " + port);
+});
